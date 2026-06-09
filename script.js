054a00dc153d94df821b928b3a52ac0afe5495cd
Xenoidaaa <sahidirma@gmail.com>
2026-06-09 19:12:15 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+/*const age = 10;
+const name= 'Dicoding';
+console.log (`Aku ${name}, umurku ${age} tahun.`);
+
+const pi = 3.14;
+console.log (pi);
+
+let username = 'Dicoding';
+console.log ('Sebelum diubah', username);
+
+username = 'DicodingAja';
+
+console.log ('Sesudah diubah', username);
+
+// Menggunakan const
+
+const username = 'Dicoding';
+console.log ('Sebelum diubah', username);
+
+username = 'DicodingAja';
+
+console.log ('Sesudah diubah', username);*/
+
+/*const currentYear = new Date().getFullYear();
+const text = `Sekarang adaalah tahun ${currentYear}.`;
+
+console.log (text);
+
+const result = Number('Dicioding');
+console.log(result);
+
+const completed = true;
+const passed = false;
+
+console.log (completed, passed);*/
+
+/*const IsGreater = 5 > 7;
+console.log (IsGreater);
+
+const name1 = { first: 'Dicoding', last: null };
+const name2 = { first: 'Dicoding'}
+
+console.log (JSON.stringify(name1));
+console.log (JSON.stringify(name2));*/
+
+/*const number = 123;
+const boolean = true;
+
+const strNumber = String(number);
+const strBoolean = boolean.toString();
+
+console.log (strNumber);
+console.log (strBoolean);*/
+
+const currensy = "IDR";
+let value = 10000;
+value += 5000;
+const money = currensy + " " + value;
+console.log (money);
+
